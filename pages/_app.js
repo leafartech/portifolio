7258c8f4a1bd85fa456fe0a139b8cb903c279e9d
leafartech <119262053+leafartech@users.noreply.github.com>
2023-05-21 17:37:39 -0300

    Delete pages directory

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../src/styles/GlobalStyle.css'
-
-export default function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Component {...pageProps} />
-        </>
-    )
-  }
