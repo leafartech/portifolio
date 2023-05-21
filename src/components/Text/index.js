92bce0ceb34e1e9364e9a4e19d94d8c2acaf8e2f
leafartech <119262053+leafartech@users.noreply.github.com>
2023-05-21 18:20:55 -0300

    Delete src directory

--- src/components/Text/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Text.module.css'
-
-export default function Text({children, tag, main, top}) {
-    const Tag = tag
-
-    return (
-        <Tag className={ `${main ? styles.mainText : ''} ${styles.text} ${top ? styles.top : ''}`}>{children}</Tag>
-    )
-}
