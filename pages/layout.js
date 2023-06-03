45c03ad73d76169f65c996ea119856462238e986
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-03 18:51:01 -0300

    Delete pages directory

--- pages/layout.js
+++
@@ -1,6 +0,0 @@
-export const metadata = {
-    title: 'Portifólio do programador Rafael Bezerra',
-    description: 'Aqui, tratamos sobre programação. Além de ser meu portifólio profissional com uma vitrine projetos que realizei, é também um Blog',
-  };
-   
-  export default function Page() {}
