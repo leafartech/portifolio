10730b74dff7a8914036e89210cf9a7259b98410
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-03 18:57:34 -0300

    Delete src directory

--- src/components/Main/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Main.module.css'
-
-export default function Main({ children, center }) {
-    return (
-        <main className={ `${center ? styles.center : ''} ${styles.main}` }>
-            {children}
-        </main>
-    )
-}
