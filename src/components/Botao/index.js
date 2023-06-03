fd1c8bbe0a4fbf7468b45367fe85aebbb50f7f07
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-03 18:50:28 -0300

    Delete src directory

--- src/components/Botao/index.js
+++
@@ -1,7 +0,0 @@
-import styles from './Botao.module.css'
-
-export default function Botao({ type, text}) {
-    return (
-        <button type={ type } className={ styles.botao }>{text}</button>
-    )
-}
