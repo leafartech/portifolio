528760e3566c544b5dd26dac2bad41686909b17a
leafartech <119262053+leafartech@users.noreply.github.com>
2023-06-23 15:01:19 -0300

    Delete src/components directory

--- src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import styles from './Button.module.css'
-
-export default function Button({children, type}) {
-    return (
-        <button className={ styles.button } type={type}>{children}</button>
-    )
-}
