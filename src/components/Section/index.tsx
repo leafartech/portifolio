5bc3ff94af1125c1980bc6936dd0935849c4553c
leafartech <119262053+leafartech@users.noreply.github.com>
2023-09-16 07:22:23 -0300

    Add files via upload

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+
+interface SectionProps {
+    children: ReactNode
+    id: string
+    className?: string
+}
+
+export default function Section({ children, className, id }: SectionProps) {
+    return (
+        <section className={`w-full flex items-center justify-center ${className}`} id={id}>
+            <div className="w-full max-w-7xl flex items-center justify-center flex-col">
+                {children}
+            </div>
+        </section>
+    )
+}
