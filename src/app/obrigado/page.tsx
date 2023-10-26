543b7b77d28cf2ce48068cb9de48ef4c8a4dec28
leafartech <119262053+leafartech@users.noreply.github.com>
2023-10-26 10:26:30 -0300

    Delete src directory

--- src/app/obrigado/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Obrigado() {
-    return (
-        <main className="flex items-center justify-center h-screen bg-zinc-50">
-            <h2 className="text-xl font-medium mb-4">Obrigado por se cadastrar</h2>
-        </main>
-    )
-}
