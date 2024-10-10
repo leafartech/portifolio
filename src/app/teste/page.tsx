516823b1e9cc55c7b992b745e04ea3f5a2b78213
leafartech <119262053+leafartech@users.noreply.github.com>
2024-10-10 11:41:09 -0300

    opa

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,25 +1,7 @@
-import HeroTeste from "@/components/HeroTeste";
-
 export default function Page() {
     return (
         <div className="">
-            <section className="w-full flex justify-center">
-                <div className="w-full max-w-7xl">
-                    <HeroTeste
-                        title="Headline principal da página para apresentar oferta"
-                        imagePath="eu"
-                    >
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </HeroTeste>
-                    <HeroTeste
-                        title="Headline principal da página para apresentar oferta"
-                        imagePath="eu"
-                        reverse={true}
-                    >
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                    </HeroTeste>
-                </div>
-            </section>
+            
         </div>
     )
 }
